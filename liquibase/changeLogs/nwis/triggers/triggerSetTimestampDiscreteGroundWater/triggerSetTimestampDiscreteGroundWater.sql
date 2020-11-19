0a8e85df7bb95db34054de747a285c06498da16a
Andrew Yan <ayan@usgs.gov>
2020-11-19 16:03:46 -0600

    add columns and triggers to the discrete_ground_water_aqts table

--- liquibase/changeLogs/nwis/triggers/triggerSetTimestampDiscreteGroundWater/triggerSetTimestampDiscreteGroundWater.sql
+++
@@ -1,4 +0,0 @@
-create trigger set_timestamp_discrete_ground_water
-before update on ${NWIS_SCHEMA_NAME}.discrete_ground_water
-for each row
-execute procedure ${NWIS_SCHEMA_NAME}.trigger_set_timestamp();
